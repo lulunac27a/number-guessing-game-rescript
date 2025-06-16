4cfe7b1d31e93d9e090508a0cacfd82225139c90
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-06-16 13:47:46 -0500

    Add Prettier and format code

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.res.mjs"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./index.html", "./src/**/*.res.mjs"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 };
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      include: ["**/*.res.mjs"],
-    }),
-  ],
+    plugins: [
+        react({
+            include: ["**/*.res.mjs"],
+        }),
+    ],
 });
